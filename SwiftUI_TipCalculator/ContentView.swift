8c155a9f1057e8f6ad41da1242ac328f3333f6ad
borikanes <ooludemi@terpmail.umd.edu>
2019-10-29 19:47:08 -0400

    started UI

--- SwiftUI_TipCalculator/ContentView.swift
+++ SwiftUI_TipCalculator/ContentView.swift
@@ -9,8 +9,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var tipPercentage = 0
+    @State private var totalAmount: String = ""
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello World")
+
+        VStack {
+            HStack{
+                Text("Total amount entered:")
+                Spacer()
+                Text("100")
+            }.padding()
+            
+            HStack{
+                Text("Tip percentage selected:")
+                Spacer()
+                Text("0")
+            }
+            .foregroundColor(.red)
+            .padding()
+            
+            HStack{
+                Text("Total amount with tip:")
+                Spacer()
+                
+                Text("100")
+            }
+            .foregroundColor(.green)
+            .padding()
+
+            TextField("Enter amount here", text: $totalAmount)
+                .keyboardType(.decimalPad) .textFieldStyle(RoundedBorderTextFieldStyle()).padding()
+            
+            Picker(selection: $tipPercentage, label: Text("Please select percentage you want to tip.")) {
+                ForEach(tipPercentages, id: \.self) { eachTipPercentage in
+                    Text(String(eachTipPercentage))
+                }
+//                Text("0%").tag(0)
+//                Text("10%").tag(1)
+//                Text("15%").tag(2)
+//                Text("18%").tag(3)
+//                Text("20%").tag(4)
+            }.pickerStyle(SegmentedPickerStyle())
+            .padding()
+        }
     }
 }
 
---
+++ SwiftUI_TipCalculator/TipCalculatorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TipCalculatorViewModel.swift
+//  SwiftUI_TipCalculator
+//
+//  Created by Bori Oludemi on 10/29/19.
+//  Copyright © 2019 Jason Wang. All rights reserved.
+//
+
+import Combine
+import SwiftUI
+
+
