28e97e80e4092b5b49bf1bc07e3ccc0d094a8ee6
Jason Wang <qwang216@gmail.com>
2019-10-29 01:43:51 -0400

    first commit

---
+++ SwiftUI_TipCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_TipCalculator
+//
+//  Created by Jason wang on 10/29/19.
+//  Copyright © 2019 Jason Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
